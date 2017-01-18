db0ebe7c3e87d6ba16f66362ba78f27aa4d5ff46
biomonika NA <biomonika@psu.edu>
2017-01-18 15:29:05 +0000

    runFeatures.sh created online with Bitbucket

---
+++ runFeatures.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -C new
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH -t 0
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=biomonika@psu.edu
+
+set -e
+set -x
+
+/galaxy/home/biomonika/R-3.2.4revised_nn/bin/Rscript runErrorStatistics_nnFeatures.R /nfs/brubeck.bx.psu.edu/scratch5/wilfried/kinetics/ $1 features10000 #provide chromosome and folder
+echo "Chromosome " $1 " done."
