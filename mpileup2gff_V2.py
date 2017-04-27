d61e956dc75ead315a36edf65fefad462aface6b
Wilfried Guiblet <Willou@client-130-203-95-209.mobility.psu.edu>
2017-04-27 10:29:27 -0400

    Apr27th update

--- mpileup2gff_V2.py
+++ mpileup2gff_V2.py
@@ -1,7 +1,10 @@
 import sys
 import re
 
-infile = open(sys.argv[1], 'rt')
+#infile = open(sys.argv[1], 'rt')
+filename = sys.argv[1]
+infile = open(filename, 'rt')
+
 
 collapsed = []
 
@@ -10,6 +13,15 @@ for line in infile:
 		line = line.strip()
 		array = line.split("\t")
 		chrom, pos, ref, alt, info = array[0], array[1], array[3], array[4], array[7]
+		if alt == "<*>":
+			DPR = re.search('[D][P][R][=][0-9,\,]+', info).group()[4:]
+                        DPR = DPR.split(',')
+			DPR = map(int, DPR)
+                        DP = sum(DPR)
+			key = str(chrom)+'|'+str(pos)+'|'+str(ref)+'|'+str(alt)+'|NoVar|'+str(DP)
+                        collapsed.append(key)
+
+
 		if alt != "<*>":
 			alt = alt.replace(',<*>', '')
 			key = str(chrom)+'|'+str(pos)+'|'+str(ref)+'|'+str(alt)+'|'
@@ -43,11 +55,17 @@ for line in infile:
 						VAR = True
 						key = key+'SNP,'
 					i += 1
-			if VAR:
+			if VAR == False:
+				key = key + 'NoVar|'+str(DP)
+				collapsed.append(key)
+
+			else:
 				key = key[:-1]	#Remove the last ","
 				key = key + '|' + str(DP)
 				collapsed.append(key)
 
 for key in collapsed:
         chrom, pos, ref, alt, VT,DP= key.split('|')
-        print(str(chrom)+'\tIllumina\t'+str(VT)+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(DP)+'\t'+str(ref)+'\t'+str(alt))
+#        print(str(chrom)+'\tIllumina\t'+str(VT)+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(DP)+'\t'+str(ref)+'\t'+str(alt))
++        print(str(chrom)+'\t'+str(basename(filename))+'\t'+str(VT)+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(DP)+'\t'+str(ref)+'\t'+str(alt))
+
--- parse_intersect.py
+++ parse_intersect.py
@@ -1,25 +1,43 @@
 import sys
+from collections import OrderedDict
 
-file = open(sys.argv[1], 'rt')
+infile = open(sys.argv[1], 'rt')
+collapsed = OrderedDict()
 
-collapsed = {}
+error = 0
 
-for line in file:
+for line in infile:
 	line = line.strip()
 	array = line.split('\t')
-	VT,AF,ref,alt,chrom, start, end = array[11], array[14], array[15],array[16],array[0], array[3], array[4]
+	VT,DP,ref,alt,chrom, start, end = array[11], array[14], array[15],array[16],array[0], array[3], array[4]
 	key = str(chrom)+'|'+str(start)+'|'+str(end)
 	if key not in collapsed:
-		collapsed[key] = []
-	if VT == 'SNP':
-		collapsed[key].append('SNP')
-	if VT == 'INS':
-		collapsed[key].append('INS')
-	if VT == 'DEL':
-		collapsed[key].append('DEL')
+		collapsed[key] = [0,0,0,0]
+	if VT == '.':
+		error += 1
+	if VT != '.':
+		collapsed[key][0] += float(DP)
+
+		SNP = VT.count('SNP')
+		INS = VT.count('INS')
+		DEL = VT.count('DEL')
+	
+		collapsed[key][1] += SNP
+		collapsed[key][2] += INS
+		collapsed[key][3] += DEL
 
 for key in collapsed:
 	chrom, start, end = key.split('|')
-	print(str(chrom)+'\t'+str(start)+'\t'+str(end)+'\t'+str(collapsed[key]))
-	
+	DP, SNP, INS, DEL = collapsed[key]
+	if int(DP) != 0:
+		SNP = float(SNP) / float(DP)
+		INS = float(INS) / float(DP)
+		DEL = float(DEL) / float(DP)
+		TOTAL = SNP + INS + DEL
+	else:
+		SNP = 0.0
+		INS = 0.0
+		DEL = 0.0
+		TOTAL = 0.0
+	print(str(chrom)+'\t'+str(start)+'\t'+str(end)+'\t'+str(TOTAL)+'\t'+str(SNP)+'\t'+str(INS)+'\t'+str(DEL))
 
