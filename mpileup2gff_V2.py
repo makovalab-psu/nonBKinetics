c7c8fdd7fc6a1f8ecb710bed6318fb195c0f942f
biomonika NA <biomonika@psu.edu>
2017-04-27 15:11:52 +0000

    mpileup2gff_V2.py edited online with Bitbucket

--- mpileup2gff_V2.py
+++ mpileup2gff_V2.py
@@ -15,19 +15,19 @@ for line in infile:
 		chrom, pos, ref, alt, info = array[0], array[1], array[3], array[4], array[7]
 		if alt == "<*>":
 			DPR = re.search('[D][P][R][=][0-9,\,]+', info).group()[4:]
-                        DPR = DPR.split(',')
+						DPR = DPR.split(',')
 			DPR = map(int, DPR)
-                        DP = sum(DPR)
+						DP = sum(DPR)
 			key = str(chrom)+'|'+str(pos)+'|'+str(ref)+'|'+str(alt)+'|NoVar|'+str(DP)
-                        collapsed.append(key)
+						collapsed.append(key)
 
 
 		if alt != "<*>":
 			alt = alt.replace(',<*>', '')
 			key = str(chrom)+'|'+str(pos)+'|'+str(ref)+'|'+str(alt)+'|'
 
-                        #DP = re.search('[D][P][=][0-9]+', info).group()[3:]
-                        DPR = re.search('[D][P][R][=][0-9,\,]+', info).group()[4:]
+			#DP = re.search('[D][P][=][0-9]+', info).group()[3:]
+			DPR = re.search('[D][P][R][=][0-9,\,]+', info).group()[4:]
 			DPR = DPR.split(',')
 			DPR = map(int, DPR)
 			DP = sum(DPR)
@@ -41,10 +41,10 @@ for line in infile:
 					freq = float(DPR[i]) / float(DP)
 					if freq <= 0.1 :
 						VAR = True
- 		                                if len(alt) > len(ref):
- 		                                        key = key+'INS,'
- 		                                else:
- 		                                        key = key+'DEL,'
+						if len(alt) > len(ref):
+							key = key+'INS,'
+						else:
+							key = key+'DEL,'
 					i += 1
 			else:
 				i = 1
@@ -66,5 +66,5 @@ for line in infile:
 
 for key in collapsed:
 	chrom, pos, ref, alt, VT,DP= key.split('|')
-#        print(str(chrom)+'\tIllumina\t'+str(VT)+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(DP)+'\t'+str(ref)+'\t'+str(alt))
+#		print(str(chrom)+'\tIllumina\t'+str(VT)+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(DP)+'\t'+str(ref)+'\t'+str(alt))
 	print(str(chrom)+'\t'+str(basename(filename))+'\t'+str(VT)+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(DP)+'\t'+str(ref)+'\t'+str(alt))
