7ca7cbf4757baa2e6adf621d1044ce48c740b404
wilfried.guiblet@gmail.com <wilfried.guiblet@gmail.com>
2017-08-07 19:28:49 +0000

    bash_diversity.sh edited online with Bitbucket

--- Diversity/bash_diversity.sh
+++ Diversity/bash_diversity.sh
@@ -8,16 +8,20 @@ export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:
 inp=$1
 outp=$2
 
-##python vcf_extract.py $inp $outp
-##cat chr* > WG
-##python WG_to_gff.py
+#####CREATE WG.gff######
+python vcf_extract.py $inp $outp
+cat chr* > WG
+python WG_to_gff.py WG
 
-##awk '$6 >= 0.05 {print $0}' WG.gff > WG_filtered.gff
 
-#grep 'SNP' /nfs/brubeck.bx.psu.edu/scratch5/wilfried/kinetics/Clean/1kG/WG_filtered.gff > WG_SNP.gff
-#python Polarize_to_gff.py ../Polarizing/WG.polarized > WG_polarized.gff
-#cat WG_SNP.gff WG_polarized.gff > WG.gff #also run temp.py to add back chr
+########POLARIZING INDELS#########
+awk '$6 >= 0.05 {print $0}' WG.gff > WG_filtered.gff
 
+grep 'SNP' /nfs/brubeck.bx.psu.edu/scratch5/wilfried/kinetics/Clean/1kG/WG_filtered.gff > WG_SNP.gff
+python Polarize_to_gff.py ../Polarizing/WG.polarized > WG_polarized.gff
+cat WG_SNP.gff WG_polarized.gff > WG.gff #also run temp.py to add back chr
+
+################################
 
 python format_to_gff.py ${inp}
 bedtools intersect -wa -wb -b WG.gff -a ${inp}.gff -loj > ${inp}.intersect
