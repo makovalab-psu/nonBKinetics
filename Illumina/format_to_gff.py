68779d2038e43c5fcfd3bac2a829c712a05a6d53
wilfried.guiblet@gmail.com <wilfried.guiblet@gmail.com>
2017-08-07 19:06:31 +0000

    format_to_gff.py edited online with Bitbucket

--- Illumina/format_to_gff.py
+++ Illumina/format_to_gff.py
@@ -4,9 +4,8 @@ infile = open(sys.argv[1], 'r')
 outfile = open(sys.argv[1]+".gff", 'w')
 
 for line in infile:
-	line=line.strip()
-	array=line.split('\t')
-	chrom, start, end, feat, length = array[0], array[1], array[2], array[3], array[4]
+        line=line.strip()
+        array=line.split('\t')
+        chrom, start, end, feat, length = array[0], array[1], array[2], array[3], array[4]
 
-	outfile.write(chrom+'\tABCC\tGQuad\t'+str(start)+'\t'+str(end)+'\t0\t+\t.\t.\n')
-
+        outfile.write(chrom[3:]+'\tABCC\tGQuad\t'+str(start)+'\t'+str(end)+'\t0\t+\t.\t.\n')
