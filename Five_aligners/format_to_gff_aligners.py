28ff773505cfad7a8626d267aa0925946dec6fff
Bob Harris <rsharris@bx.psu.edu>
2017-08-07 15:19:37 -0400

    add pre alignment script for figure S2 to repository

---
+++ Five_aligners/format_to_gff_aligners.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+infile = open(sys.argv[1], 'r')
+outfile = open(sys.argv[1]+".gff", 'w')
+
+for line in infile:
+	line=line.strip()
+	array=line.split('\t')
+	chrom, start, end, feat, length = array[0], array[1], array[2], array[3], array[4]
+
+	outfile.write(chrom+'\tABCC\tGQuad\t'+str(start)+'\t'+str(end)+'\t0\t+\t.\t.\n')
+
