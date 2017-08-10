3a0652cf2e4f38d16728d89a02c332218ba53384
biomonika NA <biomonika@psu.edu>
2017-08-10 20:05:48 +0000

    filter_out_true_variants.sh edited online with Bitbucket

--- filter_out_true_variants.sh
+++ filter_out_true_variants.sh
@@ -4,14 +4,13 @@ motif_gff=$1
 filtered_motif_gff=`basename $motif_gff`_varFiltered.gff
 filtered_motif_gff=`echo "$filtered_motif_gff" | sed 's/\.mf\.gff//g'`
 
-echo $filtered_motif_gff
 reference_vcf="HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz"
 
 export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:$PATH"
 
-bedtools intersect -u -a ${motif_gff} -b ${reference_vcf} >${filtered_motif_gff}
-echo "Filtering ${motif_gff}"
+bedtools intersect -v -a ${motif_gff} -b ${reference_vcf} >${filtered_motif_gff}
+echo -n "Filtering"
 echo -n `wc -l ${motif_gff}`
-echo "resulting in "
+echo -n "resulting in "
 echo -n `wc -l ${filtered_motif_gff}`
 echo -n "windows"
