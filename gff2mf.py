15d00ac8224550f4dbe43a9eb70f500e8e9b47ba
biomonika NA <biomonika@psu.edu>
2017-08-11 16:00:16 +0000

    gff2mf.py created online with Bitbucket

---
+++ gff2mf.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+import collections
+import os.path
+import itertools
+import operator
+
+from os.path import basename
+from itertools import izip
+from collections import defaultdict
+
+def get_arg(index):
+    try:
+        sys.argv[index]
+    except IndexError:
+        return False
+    else:
+        return sys.argv[3]
+
+######################################
+#python gff2mf.py originalmfFile gffFile
+originalMfFile=str(sys.argv[1])
+gffFile=str(sys.argv[2])
+mode=get_arg(3) #optional argument 'keep|remove' picks the subsetting mode
+######################################
+
+if(get_arg(3)==False):
+    mode="keep"
+else:
+    mode=get_arg(3)
+
+print("originalMfFile: " + originalMfFile + "; " + "gffFile: " + gffFile)
+
+if (mode=="keep"):
+    print("Mode is to subset to MATCHING LINES.")
+else:
+    print("Mode is to subset to NON-MATCHING LINES.")
+
+gffDict=defaultdict()
+outputEmpty=gffFile.replace(".gff",".mf")
+outputEmpty=basename(outputEmpty)
+
+# Do not overwrite existing file
+if (os.path.exists(outputEmpty)):
+    print("File " + outputEmpty + " already exists. Quit.")
+    sys.exit()
+
+def load_unoriginal(gffFile):
+    """load the file that represents the needed subset
+
+    Arguments:
+        gffFile input file handle 
+
+    Returns:
+        dictionary with the first three columns as a key and the rest as single value
+    """
+    with open(gffFile) as f:
+        for line_with_errors in f:
+            array_with_errors = line_with_errors.rstrip().split("\t")
+            #coordinates chr start end
+            key=str(array_with_errors[0])+" "+str(array_with_errors[3])+" "+str(array_with_errors[4])
+
+            values=True
+            gffDict[key]=values
+    f.close()
+    return gffDict
+
+def walk_original(originalMfFile, gffDict):
+    """walk the original file and subset
+
+    Arguments:
+        originalMfFile input file handle
+        dictionary with the subsetted data
+
+    """
+    original_gffDict = []
+    fout = open(outputEmpty, 'w')
+    with open(originalMfFile) as f:
+        i=0
+        for line_with_ipds in f:
+            i=i+1 #line_with_ipds number
+            array_with_ipds=line_with_ipds.rstrip().split("\t", 3)
+            left = array_with_ipds[0:3]
+            key=str(left)
+            flattenned = ' '.join( left )
+            print(flattenned)
+            if (mode=="keep"):
+                if gffDict.has_key(flattenned):
+                    fout.write(line_with_ipds)
+            else:
+                if !(gffDict.has_key(flattenned)):
+                    fout.write(line_with_ipds)
+    f.close()
+    fout.close()
+
+gffDict = load_unoriginal(gffFile)
+print(gffDict.keys())
+walk_original(originalMfFile, gffDict)
+
+
+
