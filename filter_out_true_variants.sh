9d0b61d9b4b7d07079b7023af0ff87217d7ed433
biomonika NA <biomonika@psu.edu>
2017-08-12 02:09:35 +0000

    filter_out_true_variants.sh edited online with Bitbucket

--- filter_out_true_variants.sh
+++ filter_out_true_variants.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 motif_gff=$1
-filtered_motif_gff=`basename $motif_gff`_varFiltered.gff
+filtered_motif_gff=$(basename $motif_gff)_varFiltered.gff
 filtered_motif_gff=`echo "$filtered_motif_gff" | sed 's/\.mf\.gff//g'`
+varContaining_motif_gff=$(basename $motif_gff)_varContaining.gff
+varContaining_motif_gff=`echo "$varContaining_motif_gff" | sed 's/\.mf\.gff//g'`
 
 reference_vcf="HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz"
 
 export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:$PATH"
 
 bedtools intersect -v -a ${motif_gff} -b ${reference_vcf} >${filtered_motif_gff}
-echo "Filtering "
+bedtools intersect -u -a ${motif_gff} -b ${reference_vcf} >${varContaining_motif_gff}
+echo "Filtering based on variants"
 echo -n `wc -l ${motif_gff}`
 echo " resulting in "
 echo -n `wc -l ${filtered_motif_gff}`
