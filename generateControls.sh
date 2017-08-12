a0598daffc78f42dda957ce4ed35fb10e4205293
biomonika NA <biomonika@psu.edu>
2017-08-12 19:28:31 +0000

    generateControls.sh edited online with Bitbucket

--- generateControls.sh
+++ generateControls.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
+array=(APhasedRepeats DirectRepeats InvertedRepeats MirrorRepeats ZDNAMotifs GQuadPlus GQuadMinus); 
 
-# todo
-# for a in filtered/*; do echo $a; python generateFeatures.py $a; done;
-# mkdir forward
-# mv filtered/*.mf forward/
-
-
-array=(APhasedRepeatsFeatureOnly.mf DirectRepeatsFeatureOnly.mf InvertedRepeatsFeatureOnly.mf MirrorRepeatsFeatureOnly.mf ZDNAMotifsFeatureOnly.mf GQuadPlusFeatureOnly.mf GQuadMinusFeatureOnly.mf); 
+mf_directory=$1 #directory where mf files are; we will generate 10 controls for these files
 
 #generate 10 control sets
 for i in {1..10}; do 
 echo "$i "; 
-	directory="control"${i}; mkdir -p $directory; 
-	for a in forward/*; do 
-		echo $a; python generateEmptyTrack.py $a forward/Empty.mf $directory;
+	control_directory=control_`basename $mf_directory`
+	control_directory=${control_directory}_${i}; 
+	mkdir -p $control_directory; 
+	
+	for a in ${mf_directory}/*.mf; do 
+		python generateEmptyTrack.py $a ${mf_directory}/*Empty*.mf $control_directory;
 	done;
 	
-	cd $directory 
+	cd $control_directory 
 	sed -i 's/^chr//g' *mf* #remove chr at the beginning of the lines
 
 	#sort the files into appropriate folders
 	mkdir -p tmp; 
 	for mf in "${array[@]}"; do 
-		echo ${mf}; mv ${mf}* tmp; 
+		echo ${mf}; mv *${mf}*.mf* tmp; 
 	done; 
 	mkdir -p other; mv *mf* other; 
 	mv tmp/*mf* .; rm -r tmp;
