23b96a7c6c3342ddbe5f38eb434aeeb8abf3d844
biomonika NA <biomonika@psu.edu>
2017-08-17 18:21:19 +0000

    NVC_to_gff.py edited online with Bitbucket

--- Illumina/NVC_to_gff.py
+++ Illumina/NVC_to_gff.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import argparse
+from os.path import basename
 parser = argparse.ArgumentParser()
 
 #python NVC_to_gff.py -f input.vcf
@@ -10,33 +11,32 @@ def iscomment(s): #this function is from https://stackoverflow.com/questions/173
 
 parser.add_argument("--file", "-f", type=str, required=True)
 args = parser.parse_args()
+variants = []
 
 from itertools import dropwhile
 with open(args.file, 'r') as f:
-    i=0
     for line in dropwhile(iscomment, f):
         line = re.sub('\t+','\t',line.rstrip())
-        print(line)
-        i=i+1
+        #print(line)
+
         total_depth=int(0)
         total_mismatches=int(0)
         total_insertions=int(0)
         total_deletions=int(0)
 
         chrom, pos, rid, ref, alt, qual, rfilter, info, format, annotation = line.split('\t')
-        #print (str(i)+ " " + annotation)
+
         alleles=annotation.split(':')[3:]
         alleles=str(''.join(alleles))
 
         if (str(alt)=='.'):
-            print "NoVar"
+            #print "NoVar"
+            variants.append("NoVar")
             for allele in alleles.split(',')[0:-1]:
-                print ("allele:" + allele)
                 char,depth = allele.split('=')
                 total_depth=total_depth+int(depth)
         else:
             for allele in alleles.split(',')[0:-1]:
-                print ("allele:" + allele)
                 char,depth = allele.split('=')
                 if (char==ref):
                     total_depth=total_depth+int(depth)
@@ -45,26 +45,28 @@ with open(args.file, 'r') as f:
                         if ("d1" in str(char)):
                             print("reference allele of deletion")
                         else:
-                            print("MISMATCH")
+                            #print("SNP")
+                            variants.append("SNP")
                             total_mismatches=total_mismatches+1
                             total_depth=total_depth+int(depth)
                     else: 
                         if (len(str(char))>len(str(ref))):
-                            print("INSERTION")
+                            #print("INS")
+                            variants.append("INS")
                             total_insertions=total_insertions+1
                             total_depth=total_depth+int(depth)
                         else: 
-                            print("DELETION")
+                            #print("DEL")
+                            variants.append("DEL")
                             total_deletions=total_deletions+1
                             total_depth=total_depth+int(depth)
 
 
-        print("Total depth per site:" + str(total_depth) + " | mism:" + str(total_mismatches) + " | ins:" + str(total_insertions) + " | del:" + str(total_deletions))
-        print("")
-        print("-------------------")
+        #print("Total depth per site:" + str(total_depth) + " | mism:" + str(total_mismatches) + " | ins:" + str(total_insertions) + " | del:" + str(total_deletions))
+        print(str(chrom)+'\t'+str(basename(args.file))+'\t'+str(','.join(variants))+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(total_depth)+'\t'+str(ref)+'\t'+str(alleles))
+        variants=[]
 
 
 
 
 
-#print(str(chrom)+'\t'+str(basename(filename))+'\t'+str(VT)+'\t'+str(pos)+'\t'+str(pos)+'\t'+str(DP)+'\t'+str(ref)+'\t'+str(alt))
