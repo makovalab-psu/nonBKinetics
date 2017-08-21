d37dadf8d92795f8e5f5e2556676f1a277af514f
biomonika NA <biomonika@psu.edu>
2017-08-21 16:09:16 +0000

    filter_out_repeatmasked.sh edited online with Bitbucket

--- filtering/filter_out_repeatmasked.sh
+++ filtering/filter_out_repeatmasked.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 motif_gff=$1
-RMfiltered_motif_gff=`basename $motif_gff`_RMfiltered.gff
-RMfiltered_motif_gff=`echo "$RMfiltered_motif_gff" | sed 's/\.mf\.gff//g'`
-RMcontaining_motif_gff=`basename $motif_gff`_RMcontaining.gff
-RMcontaining_motif_gff=`echo "$RMcontaining_motif_gff" | sed 's/\.mf\.gff//g'`
+repeatmasked_reference="Human_interspersed" #OR Human_rmsk
 
-repeatmasked_reference="Human_rmsk.bed"
+if [ ${motif_gff: -4} == ".gff" ]
+then
+    RMfiltered_motif_gff=`basename $motif_gff`_${repeatmasked_reference}_filtered.gff
+    RMfiltered_motif_gff=`echo "$RMfiltered_motif_gff" | sed 's/\.mf\.gff//g'`
+    RMcontaining_motif_gff=`basename $motif_gff`_${repeatmasked_reference}_containing.gff
+    RMcontaining_motif_gff=`echo "$RMcontaining_motif_gff" | sed 's/\.mf\.gff//g'`
 
-export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:$PATH"
+    export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:$PATH"
 
-bedtools intersect -v -a ${motif_gff} -b ${repeatmasked_reference} >${RMfiltered_motif_gff}
-bedtools intersect -u -a ${motif_gff} -b ${repeatmasked_reference} >${RMcontaining_motif_gff}
-echo "Filtering based on repeatmasking"
-echo -n `wc -l ${motif_gff}`
-echo " resulting in "
-echo -n `wc -l ${RMfiltered_motif_gff}`
-echo " windows"
+    bedtools intersect -v -a ${motif_gff} -b ${repeatmasked_reference}.bed >${RMfiltered_motif_gff}
+    bedtools intersect -u -a ${motif_gff} -b ${repeatmasked_reference}.bed >${RMcontaining_motif_gff}
+    echo "Filtering based on repeatmasking"
+    echo -n `wc -l ${motif_gff}`
+    echo " resulting in "
+    echo -n `wc -l ${RMfiltered_motif_gff}`
+    echo " windows"
+else
+    echo "Input file is not .gff. Abort."
+fi
