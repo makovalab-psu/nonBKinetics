0e39552c7921f6d25850d3a3ff1b2d9f595ce2c8
wilfried.guiblet@gmail.com <wilfried.guiblet@gmail.com>
2017-08-30 15:06:30 +0000

    bash_microsat.sh edited online with Bitbucket

--- MicrosatAnnotation/bash_microsat.sh
+++ MicrosatAnnotation/bash_microsat.sh
@@ -6,7 +6,7 @@
 
 export PATH="/galaxy/home/wilfried/Anaconda/bin:$PATH"
 
-python format.py all.per1.TRs.bed.10 Mono
+python format.py all.per1.TRs.bed.10 Mono #all.per1.TRs.bed.10 is the annotation file from STR-FM for mononucleotide STRs
 python line_up_microsats.py Mono > Mono.aligned
 python parse_repeats.py Mono.aligned
 for a in *n; do cut -f1,2,3,4,5 $a > ../$a; done;
