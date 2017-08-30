594aea3906eb1649543753a17b2897af914f2dbd
wilfried.guiblet@gmail.com <wilfried.guiblet@gmail.com>
2017-08-30 14:19:09 +0000

    bash_comporegress.sh created online with Bitbucket

---
+++ CompoRegress/bash_comporegress.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -C new
+#SBATCH -t 0
+#SBATCH --ntasks=8
+
+
+export PATH="/galaxy/home/wilfried/Anaconda/bin:$PATH"
+export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/samtools-1.3.1:$PATH"
+export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:$PATH"
+
+python bedformatting.py Windows_Collected_F # or Windows_Collected_R, obtained in the folder IPD
+
+bedtools getfasta -s -fi hg19.fa -bed Windows_Collected_F.bed > getFastaF
+python compo.py Windows_Collected_F.bed getFastaF > Windows_Collected_F.compo
+python split_by_feature.py Windows_Collected_F.compo
