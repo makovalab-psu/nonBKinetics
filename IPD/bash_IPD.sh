f4d4f87922dddb034f65a25544e9bbd5039348c5
wilfried.guiblet@gmail.com <wilfried.guiblet@gmail.com>
2017-08-30 13:09:07 +0000

    bash_IPD.sh edited online with Bitbucket

--- IPD/bash_IPD.sh
+++ IPD/bash_IPD.sh
@@ -2,14 +2,25 @@
 
 #SBATCH -C new
 #SBATCH -t 0
-
+#SBATCH --ntasks=8
 
 export PATH="/galaxy/home/wilfried/Anaconda/bin:$PATH"
+export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/samtools-1.3.1:$PATH"
+
+inp=$1
+
 
 source /nfs/brubeck.bx.psu.edu/scratch4/software/smrtanalysis/current/etc/setup.sh
 
-
-
+pbalign --nproc 8 $inp ../hg19.fa ${inp}.cmp.h5 --forQuiver --metrics IPD
+find ../ -name "*cmp.h5" -type f -exec cmph5tools.py merge --outFile out_all.cmp.h5 {} +
+cmph5tools.py summarize out_all.cmp.h5
+cmph5tools.py sort --deep out_all.cmp.h5
+cmph5tools.py select --where "(Reference == 'chr${inp}')" --outFile chr${inp}.cmp.h5 out_all.cmp.h5
+ipdSummary.py $inp --reference ../hg19.fa --outfile ${inp}.idp
+cat *pickle > 52X.pickle
+python cleanIPD.py 52X.pickle > 52XIPD
+python cleanDepth.py 52X.pickle > 52XDepth
 
 python ../prepare_windows.py ../GFF/WG_Clean/ > Windows_Ready
 cat Windows_Ready | env LC_ALL=C sort -k 1,1d -k 2,2n > Windows_Sorted
