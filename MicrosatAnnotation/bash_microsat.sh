0f11e19cf8a073f799524d56b4f082a9f1054e01
wilfried.guiblet@gmail.com <wilfried.guiblet@gmail.com>
2017-08-30 15:04:51 +0000

    bash_microsat.sh created online with Bitbucket

---
+++ MicrosatAnnotation/bash_microsat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -C new
+#SBATCH -t 0
+
+
+export PATH="/galaxy/home/wilfried/Anaconda/bin:$PATH"
+
+python format.py all.per1.TRs.bed.10 Mono
+python line_up_microsats.py Mono > Mono.aligned
+python parse_repeats.py Mono.aligned
+for a in *n; do cut -f1,2,3,4,5 $a > ../$a; done;
