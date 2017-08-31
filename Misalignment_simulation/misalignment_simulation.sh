90ed162d6a53d020c989cafd9e72a1eb2d947592
Bob Harris <rsharris@bx.psu.edu>
2017-08-31 16:45:20 +0000

    misalignment_simulation.sh edited online with Bitbucket

--- Misalignment_simulation/misalignment_simulation.sh
+++ Misalignment_simulation/misalignment_simulation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-featuresDir=$1  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/RM"
-readLength=$2   # e.g. 100
+mockName=$1     # e.g. "pomegranate"
+featuresDir=$2  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/RM"
+readLength=$3   # e.g. 100
 
 # prepare input files
 
-create_mock_genome.sh ${featuresDir} ${readLength}
+create_mock_genome.sh ${mockName} ${featuresDir} ${readLength}
 
