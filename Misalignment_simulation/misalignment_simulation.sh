7cda10d9819ae54866120519fd39591e61cb3d2d
Bob Harris <rsharris@bx.psu.edu>
2017-08-31 16:41:33 +0000

    misalignment_simulation.sh edited online with Bitbucket

--- Misalignment_simulation/misalignment_simulation.sh
+++ Misalignment_simulation/misalignment_simulation.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-bamFile=$1      # e.g. "bam/HG002.hs37d5.60x.1.bam"
-featuresDir=$2  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/RM"
-controlsDir=$3  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/control_RM_1"
-chrom=$4        # e.g. "${chrom}"
+featuresDir=$1  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/RM"
+readLength=$2   # e.g. 100
 
 # prepare input files
 
-step_1.sh ${featuresDir} ${controlsDir} ${chrom}
+create_mock_genome.sh ${featuresDir} ${readLength}
 
