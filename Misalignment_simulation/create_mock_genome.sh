8375eb7befbdee746043083809b5d6955d2f824f
Bob Harris <rsharris@bx.psu.edu>
2017-08-31 16:45:01 +0000

    create_mock_genome.sh edited online with Bitbucket

--- Misalignment_simulation/create_mock_genome.sh
+++ Misalignment_simulation/create_mock_genome.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-featuresDir=$1  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/RM"
-readLength=$2   # e.g. 100
+mockName=$1     # e.g. "pomegranate"
+featuresDir=$2  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/RM"
+readLength=$3   # e.g. 100
 
 featureList="APhasedRepeats DirectRepeats GQuadPlus GQuadMinus InvertedRepeats MirrorRepeats ZDNAMotifs"
 
@@ -12,7 +13,7 @@ featureList="APhasedRepeats DirectRepeats GQuadPlus GQuadMinus InvertedRepeats M
 
 twoBitInfo ${hg19}/seq/hg19.2bit /dev/stdout \
   | awk '{ print $1,"0",$2 }' \
-  > temp.features5.hg19.clippers
+  > temp.${mockName}.hg19.clippers
 
 separatorLen=${readLength}
 separator=`echo ${readLength} \
@@ -22,8 +23,8 @@ separator=`echo ${readLength} \
 
 # main loop to create the mock genome
 
-:> targets/features5.fa
-:> temp.features5.target.map.xxx
+:> targets/${mockName}.fa
+:> temp.${mockName}.target.map.xxx
 fakeChrNum=0
 echo ${featureList} | tr " " "\n" \
   | while read featureSpec ; do
@@ -36,73 +37,72 @@ echo ${featureList} | tr " " "\n" \
       #
       cat ${featureFile} \
         | dilate_intervals $((readLength-1)) \
-        | clip_intervals --sorted temp.features5.hg19.clippers \
+        | clip_intervals --sorted temp.${mockName}.hg19.clippers \
         | awk '{ print $1":"$2"-"$3 }' \
-        > temp.features5.${feature}.ucsc
+        > temp.${mockName}.${feature}.ucsc
       twoBitToFa ${hg19}/seq/hg19.2bit /dev/stdout \
-            -noMask -seqList=temp.features5.${feature}.ucsc \
-        > temp.features5.${feature}.fa.xxx
+            -noMask -seqList=temp.${mockName}.${feature}.ucsc \
+        > temp.${mockName}.${feature}.fa.xxx
       #
       fakeStart=0
-      :> temp.features5.${feature}.map.txt
+      :> temp.${mockName}.${feature}.map.txt
       echo ">${fakeChr} ${feature}+$((readLength-1))bp_flanks" \
-        > temp.features5.${feature}.fa
-      cat temp.features5.${feature}.fa.xxx \
+        > temp.${mockName}.${feature}.fa
+      cat temp.${mockName}.${feature}.fa.xxx \
         | fasta_to_one_line --nosep --noarrow \
         | tr ":-" "  " \
         | while read chr start end nucs ; do
             #echo "=== ${feature} ${chr} ${start} ${end} to ${fakeChr} ${fakeStart} ==="
             echo ${fakeChr} ${fakeStart} ${chr} ${start} \
-              >> temp.features5.${feature}.map.txt
-            echo ${nucs}      >> temp.features5.${feature}.fa
-            echo ${separator} >> temp.features5.${feature}.fa
+              >> temp.${mockName}.${feature}.map.txt
+            echo ${nucs}      >> temp.${mockName}.${feature}.fa
+            echo ${separator} >> temp.${mockName}.${feature}.fa
             nucsLen=`echo ${nucs} | awk '{ print length($0) }'`
             fakeStart=$((fakeStart+nucsLen+separatorLen))
             done
       #
-      cat temp.features5.${feature}.fa \
-        | fasta_set_line_length 100 \
-        >> targets/features5.fa
+      cat temp.${mockName}.${feature}.fa \
+        >> targets/${mockName}.fa
       #
-      cat temp.features5.${feature}.map.txt \
-        >> temp.features5.target.map.xxx
+      cat temp.${mockName}.${feature}.map.txt \
+        >> temp.${mockName}.target.map.xxx
       #
-      rm temp.features5.${feature}.ucsc
-      rm temp.features5.${feature}.fa.xxx
-      rm temp.features5.${feature}.fa
-      rm temp.features5.${feature}.map.txt
+      rm temp.${mockName}.${feature}.ucsc
+      rm temp.${mockName}.${feature}.fa.xxx
+      rm temp.${mockName}.${feature}.fa
+      rm temp.${mockName}.${feature}.map.txt
       done
 
 # finish the interval mapping files
 
-cat temp.features5.target.map.xxx \
-  > targets/features5.map.txt
-rm temp.features5.target.map.xxx  
+cat temp.${mockName}.target.map.xxx \
+  > targets/${mockName}.map.txt
+rm temp.${mockName}.target.map.xxx  
 
-cat targets/features5.map.txt \
+cat targets/${mockName}.map.txt \
   | awk '{ print $3,$4,$1,$2 }' \
-  > targets/features5.reverse_map.txt
+  > targets/${mockName}.reverse_map.txt
 
 # build the index(es) ...
 
-bwa index -a bwtsw targets/features5.fa
+bwa index -a bwtsw targets/${mockName}.fa
 
-samtools faidx targets/features5.fa
+samtools faidx targets/${mockName}.fa
 
 # convert to 2bit so I can do easy lookups ...
 
-faToTwoBit targets/features5.fa targets/features5.2bit
+faToTwoBit targets/${mockName}.fa targets/${mockName}.2bit
 
 # make a list of sam @SQ header records ...
 
-cat targets/features5.map.txt \
+cat targets/${mockName}.map.txt \
   | awk '{ print $3 }' \
   | sort -u \
-  > temp.features5.hg19.chroms
+  > temp.${mockName}.hg19.chroms
 
 twoBitInfo ${hg19}/seq/hg19.2bit /dev/stdout \
-  | select_lines_by_name temp.features5.hg19.chroms \
+  | select_lines_by_name temp.${mockName}.hg19.chroms \
   | awk '{ print "@SQ SN:"$1" LN:"$2 }' \
   | tr " " "\t" \
-  > targets/features5.map.sq_headers
+  > targets/${mockName}.map.sq_headers
 
