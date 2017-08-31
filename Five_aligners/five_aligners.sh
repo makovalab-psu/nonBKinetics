efb5fcca97f6ddc1c00caaf08a9b0ecde696f425
Bob Harris <rsharris@bx.psu.edu>
2017-08-31 16:26:15 +0000

    five_aligners.sh edited online with Bitbucket

--- Five_aligners/five_aligners.sh
+++ Five_aligners/five_aligners.sh
@@ -3,7 +3,7 @@
 bamFile=$1      # e.g. "bam/HG002.hs37d5.60x.1.bam"
 featuresDir=$2  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/RM"
 controlsDir=$3  # e.g. "/nfs/brubeck.bx.psu.edu/scratch6/monika/true_variants/control_RM_1"
-chrom=$4        # e.g. "${chrom}"
+chrom=$4        # e.g. "chr1"
 
 # prepare input files
 
