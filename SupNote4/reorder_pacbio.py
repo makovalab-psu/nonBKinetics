a6e7033d7c447165c93359efb09d0a597315f382
wilfried.guiblet@gmail.com <wilfried.guiblet@gmail.com>
2018-10-25 13:43:32 +0000

    reorder_pacbio.py edited online with Bitbucket

--- SupNote4/reorder_pacbio.py
+++ SupNote4/reorder_pacbio.py
@@ -22,14 +22,37 @@ motifFileWithErrors=str(sys.argv[2])
 print("motifFileWithIPds: " + motifFileWithIPds + "; " + "motifFileWithErrors: " + motifFileWithErrors)
 
 errorDict=defaultdict()
-outputEmpty=motifFileWithErrors.replace("merged_","ordered_")
-outputEmpty=basename(outputEmpty)
-outputEmpty=("reordered." + outputEmpty)
+windowCount=defaultdict(int) #number of windows is integer
+outputEmpty=motifFileWithErrors.replace("merged_","")
+output_type=".collapsed"
+
+if ("collapsed" in motifFileWithErrors):
+    output_type=".reorder"
+
+outputEmpty=re.sub('$', output_type, outputEmpty)
 
 # Do not overwrite existing file
 if (os.path.exists(outputEmpty)):
     print("File " + outputEmpty + " already exists. Quit.")
     sys.exit()
+
+def get_window_counts(motifFileWithIPds):
+    """walk the .mf file and save information about how many times each window occurs
+
+    Arguments:
+        motifFileWithIPds input .mf file
+
+    Returns:
+        dictionary with the number of times each window occurs
+    """
+    with open(motifFileWithIPds) as f:
+        for line_with_ipds in f:
+            array_with_ipds=line_with_ipds.rstrip().split(None, 3)
+            left = array_with_ipds[0:3]
+            key=str(left)
+            windowCount[key] += 1
+    f.close()
+    return windowCount
 
 def load_unordered(motifFileWithErrors):
     """load the file that needs reordering
@@ -49,7 +72,7 @@ def load_unordered(motifFileWithErrors):
     f.close()
     return errorDict
 
-def walk_ordered(motifFileWithIPds, errorDict):
+def walk_ordered(motifFileWithIPds, errorDict, windowCount):
     """walk the ordered file and save the unordered values in order
 
     Arguments:
@@ -69,8 +92,23 @@ def walk_ordered(motifFileWithIPds, errorDict):
             key=str(left)
             if errorDict.has_key(key):
                 values=errorDict[key]
-            if values:
-                left.append(values[0])
+            try:
+                if values:
+                    number_of_window_occurencies=windowCount[key]
+                    if (number_of_window_occurencies==1):
+                        left.append(values[0])
+                    else:
+                        adjusted_rates=[] #the error rates need to be adjusted because control window is present multiple times
+                        for rate in values[0].split('\t'):
+                            numerical_rate=float(rate)
+                            adjusted_rate=numerical_rate/float(number_of_window_occurencies) #adjustment
+                            adjusted_rates.append(str(adjusted_rate))
+                        adjusted_rates='\t'.join(adjusted_rates)
+                        left.append(adjusted_rates)
+
+            except UnboundLocalError:
+                print("Looks like the coordinate lookup was unsuccessfull. Are you sure you are using correct input files? Aborting.")
+                sys.exit()
             flattenned = '\t'.join( left )
             ordered_errorDict.append( flattenned )
     f.close()
@@ -78,11 +116,17 @@ def walk_ordered(motifFileWithIPds, errorDict):
 
 
 errorDict = load_unordered(motifFileWithErrors)
-ordered_errorDict = walk_ordered(motifFileWithIPds, errorDict)
+windowCount = get_window_counts(motifFileWithIPds)
+ordered_errorDict = walk_ordered(motifFileWithIPds, errorDict, windowCount)
 
 
 # Write results
 fout = open(outputEmpty, 'w')
 for item in ordered_errorDict:
-    fout.write(item + '\n')
+    #fout.write(item + '\n')
+    out_item=str(item)
+    out_item=item.split(" ")
+    if (output_type==".collapsed"):
+        out_item=out_item[0:4] #remove last 4 columns as they are not needed in plotting script working with .collapsed files
+    fout.write(str(' '.join(out_item)) + '\n')
 fout.close()
